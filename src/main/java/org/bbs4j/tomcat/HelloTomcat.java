a0e706b5fc1e8f1dba723efff452ffe1dc490e1b
x_mac <741342093@qq.com>
2017-05-27 14:39:41 +0800

    init

---
+++ src/main/java/org/bbs4j/tomcat/HelloTomcat.java
@@ -0,0 +1,10 @@
+package org.bbs4j.tomcat;
+
+/**
+ * Created by apple on 17/5/27.
+ */
+public class HelloTomcat {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
